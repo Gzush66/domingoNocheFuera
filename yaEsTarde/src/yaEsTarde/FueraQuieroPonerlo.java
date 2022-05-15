83f8f1ae367cdd2f24a919824d3c0163e384c362
Juan Jesus Lozano <garbagetin1@gmail.com>
2022-05-16 01:59:38 +0200

    solo pasamos gitignore y archivo

---
+++ yaEsTarde/src/yaEsTarde/FueraQuieroPonerlo.java
@@ -0,0 +1,10 @@
+package yaEsTarde;
+
+public class FueraQuieroPonerlo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("vamos a poner el git fuera");
+	}
+
+}
