e92f2b6ef27886358af3a426c7949e3ce759b92b
Juan Jesus Lozano <garbagetin1@gmail.com>
2022-05-16 02:01:26 +0200

    agregamos una linea

--- yaEsTarde/src/yaEsTarde/FueraQuieroPonerlo.java
+++ yaEsTarde/src/yaEsTarde/FueraQuieroPonerlo.java
@@ -5,6 +5,7 @@ public class FueraQuieroPonerlo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("vamos a poner el git fuera");
+System.out.println("solo pasamos gitignore y este archivo");
 	}
 
 }
